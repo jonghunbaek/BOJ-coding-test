cb79571943e4fd08f9a0354933653fa596982710
ahlight <107451991+jonghunbaek@users.noreply.github.com>
2023-01-03 16:39:18 +0900

    [Silver III] Title: 스택 수열, Time: 996 ms, Memory: 106368 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,56 +1,40 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
-
 public class Main {
-
-	static int n;
-	static int[] arr;
-	static List<String> res;
-	static Stack<Integer> stack;
-	static int max = 1;
-	static boolean isCheckSequence = true;
-
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt();
-		arr = new int[n];
-		res = new ArrayList<String>();
-		stack = new Stack<Integer>();
-		
-		for (int i=0; i<n; i++) {
-			int sequence = sc.nextInt();
-			if (sequence >= max) {
-				for (int j=max; j<=sequence; j++) {
-					stack.push(j);
-					res.add("+");
-					max++;
-				}
-				
-				stack.pop();
-				res.add("-");
-				
-			} else if (sequence < max) {
-				if (stack.peek() != sequence) {
-					isCheckSequence = false;
-				} 
-				
-				stack.pop();
-				res.add("-");
-			}
-		}
-		
-		if (!isCheckSequence) {
-			System.out.println("NO");
-			return;
-		}
-		
-		for (int i=0; i<res.size(); i++) {
-			System.out.println(res.get(i));
-		}
-	}
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int[]A = new int[N];
+    for (int i = 0; i < N; i++) {
+      A[i] = sc.nextInt();
+    }
+    Stack<Integer> stack = new Stack<>();
+    StringBuffer bf = new StringBuffer();
+    int num = 1; // 오름차순 수
+    boolean result = true;
+    for (int i = 0; i < A.length; i++) {
+      int su = A[i]; // 현재 수열의 수
+      if (su >= num) { //현재 수열 값 >= 오름차순 자연수 : 값이 같아 질 때까지 push()수행
+        while (su >= num) { // push()
+          stack.push(num++);
+          bf.append("+\n");
+        }
+        stack.pop();
+        bf.append("-\n");
+      }
+      else {  //현재 수열 값 < 오름차순 자연수: pop()을 수행하여 수열 원소를 꺼냅니다
+        int n = stack.pop();
+        // 스택의 가장 위의 수가 만들어야 하는 수열의 수 보다 크다면 수열 출력 불가능
+        if (n > su) {
+          System.out.println("NO");
+          result = false;
+          break;
+        } //
+        else {
+          bf.append("-\n");
+        }
+      }
+    }
+    if (result) System.out.println(bf.toString());
+  }
 }
