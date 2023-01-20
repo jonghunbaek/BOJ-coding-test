86051063141e5b8288b7206866f30974131935d5
ahlight <107451991+jonghunbaek@users.noreply.github.com>
2023-01-20 22:59:05 +0900

    [Platinum V] Title: 버블 소트, Time: 564 ms, Memory: 72884 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.java
+++ 백준/Platinum/1517. 버블 소트/버블 소트.java
@@ -3,58 +3,68 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+
 public class Main {
-  public static int[] A, tmp;
-  public static long result;
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    A = new int[N + 1];
-    tmp = new int[N + 1];
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    for (int i = 1; i <= N; i++) {
-      A[i] = Integer.parseInt(st.nextToken());
-    }
-    result = 0;
-    merget_sort(1, N); //병합정렬 수행하기
-    System.out.println(result);
-  }
 
-  private static void merget_sort(int s, int e) { 
-    if (e - s < 1)
-      return;
-    int m = s + (e - s) / 2;
-    //재귀함수 형태로 구현
-    merget_sort(s, m); 
-    merget_sort(m + 1, e);
-    for (int i = s; i <= e; i++) {
-      tmp[i] = A[i];
-    }
-    int k = s;
-    int index1 = s;
-    int index2 = m + 1;
-    while (index1 <= m && index2 <= e) {  //두 그룹을 Merge 해주는 로직 
-      if (tmp[index1] > tmp[index2]) {
-        A[k] = tmp[index2];
-        result = result + index2 - k; // 뒤쪽 데이터 값이 작아 선택되는 경우 결과 값 업데이트
-        k++;
-        index2++;
-      } else {
-        A[k] = tmp[index1];
-        k++;
-        index1++;
-      }
-    }
-    while (index1 <= m) {
-      A[k] = tmp[index1];
-      k++;
-      index1++;
-    }
-    while (index2 <= e) {
-      A[k] = tmp[index2];
-      k++;
-      index2++;
-    }
-
-  }
+	static int N;
+	static long result;
+	static int[] arr;
+	static int[] temp;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N+1];
+		temp = new int[N+1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i=1; i<=N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken()); 
+		}
+		mergeSort(1, N);
+		
+		System.out.println(result);
+	}
+	
+	public static void mergeSort(int start, int end) {
+		if (end-start<1) {
+			return;
+		}
+		
+		int mid = start + (end-start)/2;
+		mergeSort(start, mid);
+		mergeSort(mid+1, end);
+		
+		for (int i=start; i<=end; i++) {
+			temp[i] = arr[i];
+		}
+		
+		int k = start;
+		int i = start;
+		int j = mid + 1;
+		while (i<=mid && j<=end) {
+			if (temp[i] > temp[j]) {
+				arr[k] = temp[j];
+				result += j-k;
+				k++;
+				j++;
+			} else {
+				arr[k] = temp[i];
+				k++;
+				i++;
+			}
+		}
+		
+		while (i<=mid) {
+			arr[k] = temp[i];
+			k++;
+			i++;
+		}
+		
+		while (j<=end) {
+			arr[k] = temp[j];
+			k++;
+			j++;
+		}
+	}
 }
