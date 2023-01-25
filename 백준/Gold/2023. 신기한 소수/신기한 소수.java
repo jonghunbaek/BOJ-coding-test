ce4ba3d1782bb5e80e8fdbaedd4b09b623526fb0
ahlight <107451991+jonghunbaek@users.noreply.github.com>
2023-01-25 19:19:02 +0900

    [Gold V] Title: 신기한 소수, Time: 872 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,43 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.util.*;
 public class Main {
-
-	static int N;
-	static int[] primeNums = {2,3,5,7};
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-
-		for (int i=0; i<primeNums.length; i++) {
-			StringBuilder sb = new StringBuilder();
-			dfs(sb.append(primeNums[i]));
-		}
-	}
-	
-	public static void dfs(StringBuilder num) {
-		if (num.length() == N) {
-			System.out.println(num);
-			return;
-		}
-
-		for (int i=1; i<10; i++) {
-			int temp = Integer.parseInt(num.append(i).toString());
-			boolean isPrime = true;
-			
-			for (int j=2; j<temp; j++) {
-				if (temp%j == 0) {
-					isPrime = false;
-					break;
-				}
-			}
-			
-			if (isPrime) {
-				dfs(num);
-			}
-			num.deleteCharAt(num.length()-1);
-		}
-	}
+  static int N;
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    N = in.nextInt();
+    // 일의 자리 소수는 2 3 5 7 이므로 4개 수에서만 시작
+    DFS(2, 1);
+    DFS(3, 1);
+    DFS(5, 1);
+    DFS(7, 1);
+  }
+  static void DFS(int number, int jarisu) {
+    if (jarisu == N) {
+      if (isPrime(number)) {
+        System.out.println(number);
+      }
+      return;
+    }
+    for (int i = 1; i < 10; i++) {
+      if (i % 2 == 0) { // 짝수이면 더 이상 탐색할 필요가 없음
+        continue;
+      }
+      if (isPrime(number * 10 + i)) { // 소수이면 재귀로 자리수를 늘려갑니다.
+        DFS(number * 10 + i, jarisu + 1);
+      }
+    }
+  }
+  static boolean isPrime(int num) {
+    for (int i = 2; i <= num / 2; i++)
+      if (num % i == 0)
+        return false;
+    return true;
+  }
 }
