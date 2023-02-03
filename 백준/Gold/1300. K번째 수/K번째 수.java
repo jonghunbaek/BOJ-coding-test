dfd56a43edeb26ffe708e4b8a51c3924834d9c7c
ahlight <107451991+jonghunbaek@users.noreply.github.com>
2023-02-03 21:13:13 +0900

    [Gold II] Title: K번째 수, Time: 164 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,26 +1,29 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 public class Main {
-  public static void main(String[] args) throws Exception {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    int K = sc.nextInt();
-    long start = 1, end = K;
-    long ans = 0;
-    // 이분 탐색 수행
-    while (start <= end) {
-      long middle = (start + end) / 2; 
-      long cnt = 0;
-      // 중간 값보다 작은 수는 몇 개인지 계산.
-      for (int i = 1; i <= N; i++) {
-        cnt += Math.min(middle / i, N);  // 작은 수를 카운트하는 핵심로직
-      }
-      if (cnt < K) {
-        start = middle + 1;
-      } else {
-        ans = middle;  // 현재 단계의 중간 값을 정답 변수에 저장
-        end = middle - 1;
-      }
-    }
-    System.out.println(ans);
-  }
+	static int N,k;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		k = Integer.parseInt(br.readLine());
+		
+		long start = 1;
+		long end = k;
+		long res = 0;
+		while (start <= end) {
+			long mid = (start+end)/2;
+			long cnt = 0;
+			for (int i=1; i<=N; i++) {
+				cnt += Math.min(mid/i, N);
+			}
+			if (cnt < k) {
+				start = mid + 1;
+				continue;
+			} 
+			res = mid;
+			end = mid - 1;
+		}
+		System.out.println(res);
+	}
 }
