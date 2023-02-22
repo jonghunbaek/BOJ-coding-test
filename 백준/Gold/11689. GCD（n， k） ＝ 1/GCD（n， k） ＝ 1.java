68f398a56ad48fa47a55051163e4565c99875cd5
ahlight <107451991+jonghunbaek@users.noreply.github.com>
2023-02-22 22:07:23 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 132 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -1,20 +1,23 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    long n = Long.parseLong(br.readLine());
-    long result = n;
-    for (long p = 2; p <= Math.sqrt(n); p++) { // 제곱근까지만 진행
-      if (n % p == 0) { // p가 소인수인지 확인
-        result = result - result / p; // 결과 값 업데이트
-        while (n % p == 0) { // 해당 소인수를 지워줌 2^7*11이라면 2^7을 없애고 11만 남김
-          n /= p;
-        }
-      }
-    }
-    if (n > 1) // 아직 소인수 구성이 남아있는 경우
-//(반복문에서 제곱근까지만 탐색했기 때문에 1개의 소인수가 누락되는 케이스)
-      result = result - result / n;
-    System.out.println(result);
-  }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(br.readLine());
+		long temp = n;
+		for (long i=2; i<=Math.sqrt(n); i++) {
+			if (n%i == 0) {
+				temp = temp - (temp/i);
+				while (n%i == 0) {
+					n /= i;
+				}
+			}
+		}
+		if (n > 1) {
+			temp = temp - (temp/n);
+		}
+        System.out.println(temp);
+	}
 }
