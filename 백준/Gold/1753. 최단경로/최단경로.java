370a2d3daaa8f7a8b1128a1e1937d2ea548112e2
ahlight <107451991+jonghunbaek@users.noreply.github.com>
2023-04-03 21:01:47 +0900

    [Gold IV] Title: 최단경로, Time: 740 ms, Memory: 107216 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,88 +1,80 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
+    static ArrayList[] l;
+    static int[] dist;
 
-	static int V,E,start;
-	static int[] shortest;
-	static List<Node>[] nodes;
-	static boolean[] visited;
-	
-	static class Node implements Comparable<Node> {
-		int node;
-		int edge;
-		public Node (int node, int edge) {
-			this.node = node;
-			this.edge = edge;
-		}
-		
-		@Override
-		public int compareTo(Node o) {
-			return this.edge - o.edge;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		start = Integer.parseInt(br.readLine());
-		shortest = new int[V+1];
-		nodes = new ArrayList[V+1];
-		
-		Arrays.fill(shortest, 10000000);
-		shortest[start] = 0;
-		for (int i=1; i<=V; i++) {
-			nodes[i] = new ArrayList<Node>();
-		}
-		for (int i=1; i<=E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			nodes[u].add(new Node(v, w));
-		}
-		
-		dijkstra();
-		for (int i=1; i<=V; i++) {
-			if (i == start) {
-				System.out.println(0);
-				continue;
-			}
-			if (!visited[i]) {
-				System.out.println("INF");
-				continue;
-			}
-			System.out.println(shortest[i]);
-		}
-	}
-	
-	public static void dijkstra() {
-		visited = new boolean[V+1];
-		PriorityQueue<Node> q = new PriorityQueue<Node>();
-		q.add(new Node(start, 0));
-		//visited[start] = true;
-		
-		while (!q.isEmpty()) {
-			Node now = q.poll();
-			if (visited[now.node]) {
-				continue;
-			}
-			visited[now.node] = true;
-			for (Node next : nodes[now.node]) {
-				/*if (!visited[next.node]) {*/
-					shortest[next.node] = Math.min(shortest[next.node], shortest[now.node] + next.edge);
-					q.add(new Node(next.node, shortest[next.node]));	
-					//visited[next.node] = true;
-				//}
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine()) - 1;
+
+        l = new ArrayList[V];
+        dist = new int[V];
+
+        for(int i = 0; i<V; i++) {
+            l[i] = new ArrayList<Node>();
+            dist[i] = Integer.MAX_VALUE;
+        }
+
+        for(int i = 0; i<E; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int u = Integer.parseInt(st.nextToken()) - 1;
+            int v = Integer.parseInt(st.nextToken()) - 1;
+            int w = Integer.parseInt(st.nextToken());
+
+            l[u].add(new Node(v, w));
+        }
+
+        dijkstra(K);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<V; i++){
+            if(dist[i] == Integer.MAX_VALUE)
+                sb.append("INF").append("\n");
+            else
+                sb.append(dist[i]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static void dijkstra(int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        dist[start] = 0;
+        pq.add(new Node(start, 0));
+
+        while(!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            int len = l[now.v].size();
+            for(int i = 0; i<len; i++){
+                Node next = (Node)l[now.v].get(i);
+
+                if(dist[next.v]>now.w + next.w){
+                    dist[next.v] = now.w + next.w;
+                    pq.add(new Node(next.v, dist[next.v]));
+                }
+            }
+        }
+    }
+}
+
+class Node implements Comparable<Node>{
+    int v, w;
+
+    public Node(int v, int w){
+        this.v = v;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Node n){
+        return this.w - n.w;
+    }
 }
