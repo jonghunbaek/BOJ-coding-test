4d954649a0420bde72fc320d77554a9859474caa
ahlight <107451991+jonghunbaek@users.noreply.github.com>
2023-04-13 21:45:14 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 1044 ms, Memory: 96160 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
@@ -1,81 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
+ 
 public class Main {
-
-	static int n,m,k;
-	static List<Node>[] nodes;
-	static PriorityQueue<Integer>[] dijkstra;
-	
-	static class Node implements Comparable<Node>{
-		int node;
-		int val;
-		public Node(int node, int val) {
-			this.node = node;
-			this.val = val;
-		}
-		
-		@Override
-		public int compareTo(Node o) {
-			return this.val - o.val;
-		}
-	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		nodes = new ArrayList[n+1];
-		dijkstra = new PriorityQueue[n+1];
-		for (int i=1; i<=n; i++) {
-			nodes[i] = new ArrayList<Node>();
-			dijkstra[i] = new PriorityQueue<Integer>(Collections.reverseOrder());
-		}
-		
-		for (int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int val = Integer.parseInt(st.nextToken());
-			nodes[start].add(new Node(end, val));
-		}
-		
-		dijkstra();
-		for (int i=1; i<=n; i++) {
-			if (dijkstra[i].size() == k) {
-				System.out.println(dijkstra[i].peek());
-			} else {
-				System.out.println(-1);
-			}
-		}
-	}
-	
-	public static void dijkstra() {
-		PriorityQueue<Node> pq = new PriorityQueue<Node>();
-		pq.add(new Node(1, 0));
-		dijkstra[1].add(0);
-		
-		while (!pq.isEmpty()) {
-			Node now = pq.poll();
-
-			for (Node next : nodes[now.node]) {
-				if (dijkstra[next.node].size() < k) {
-					dijkstra[next.node].add(now.val+next.val);
-					pq.add(new Node(next.node, now.val+next.val));
-				} else if (dijkstra[next.node].peek() > now.val+next.val) {
-					dijkstra[next.node].poll();
-					dijkstra[next.node].add(now.val+next.val);
-					pq.add(new Node(next.node, now.val+next.val));
-				}
-			}
-		}
-	}
+ 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int N, M, K;
+    private static List<Node>[] edge;
+    private static PriorityQueue<Integer>[] dist;
+ 
+    static class Node implements Comparable<Node> {
+        int v, weight;
+ 
+        public Node(int v, int weight){
+            this.v = v;
+            this.weight = weight;
+        }
+ 
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+ 
+        init();
+ 
+        for (int i = 0; i < M; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            edge[u].add(new Node(v, weight));
+        }
+ 
+        dijkstra(1);
+        print();
+ 
+        br.close();
+    }
+ 
+    private static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+        dist[start].add(0);
+ 
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+ 
+            for (Node next : edge[cur.v]) {
+                if (dist[next.v].size() < K) { // 저장된 최단비용의 수가 K개 이하
+                    dist[next.v].add((cur.weight + next.weight) * -1);
+                    pq.add(new Node(next.v, cur.weight + next.weight));
+                } else if ((dist[next.v].peek()*-1) > cur.weight + next.weight) {
+                    dist[next.v].poll();
+                    dist[next.v].add((cur.weight + next.weight) * -1);
+                    pq.add(new Node(next.v, cur.weight + next.weight));
+                }
+            }
+        }
+    }
+ 
+    private static void print() {
+        for (int i = 1; i <= N; ++i){
+            if (dist[i].size() == K) System.out.println(dist[i].peek() * -1);
+            else System.out.println(-1);
+        }
+    }
+ 
+    private static void init() {
+        dist = new PriorityQueue[N + 1];
+        edge = new ArrayList[N + 1];
+        for (int i = 0 ; i < N + 1; ++i) {
+            dist[i] = new PriorityQueue<>(K);
+            edge[i] = new ArrayList<>();
+        }
+    }
 }
