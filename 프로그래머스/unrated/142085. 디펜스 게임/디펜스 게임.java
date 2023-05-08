0d87aa1208d60b74ecb0bb62da0c5bae6ebddbf8
ahlight <107451991+jonghunbaek@users.noreply.github.com>
2023-05-08 17:56:26 +0900

    [unrated] Title: 디펜스 게임, Time: 0.61 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -4,6 +4,10 @@ class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
         
+        if (enemy.length <= k) {
+            return enemy.length;
+        }
+        
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
         for (int i=0; i<enemy.length; i++) {
             pq.add(enemy[i]);
